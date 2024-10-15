8ca0354e88ad3b3eb61460090eff4d48210cb051
Indull <Indull0@users.noreply.github.com>
2024-10-15 23:29:43 +0200

    THE VISION CONES ARE WORKING!!

--- main.gd
+++ main.gd
@@ -19,7 +19,7 @@ func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
 	var colors = PackedColorArray([color])
 
 	for i in range(nb_points + 1):
-		var angle_point = deg_to_rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
+		var angle_point = angle_from + i * (angle_to - angle_from) / nb_points
 		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
 	draw_polygon(points_arc, colors)
 
